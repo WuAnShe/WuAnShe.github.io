012f4c9bd76b509d834109585af5d7067204c0e8
AzureXH <13750047131@163.com>
2020-11-03 00:51:56 +0800

    mainfest的大括号还是换行吧，不然看的眼睛晕

--- mainfest.js
+++ mainfest.js
@@ -1,313 +1,351 @@
 const mainfest = {
   //小东
-  kohiList: [{
-    titleCh: '吃货小东',
-    titleJp: '吃货小东',
-    voice: [
-      {
-        desCh: "介绍一下商品",
-        desJp: "商品の説明をします",
-        url: "kohigashihitona/6 (1).mp3"
-      },
-      {
-        desCh: "人鱼我选择和风汉堡肉",
-        desJp: "ひとなおろしハンバーグだったじゃん、和風の",
-        url: "kohigashihitona/6.mp3"
-      },
-      {
-        desCh: "微波炉用500瓦热1分40秒",
-        desJp: "微波炉用500瓦热1分40秒",
-        url: "kohigashihitona/7 (1).mp3"
-      },
-      {
-        desCh: "711原创副食用商品",
-        desJp: "711原创副食用商品",
-        url: "kohigashihitona/9 (2).mp3"
-      },
-      {
-        desCh: "热完后渐渐变凉的时候",
-        desJp: "热完后渐渐变凉的时候",
-        url: "kohigashihitona/9.mp3"
-      }
-    ]
-  }, {
-    titleCh: '憨憨操作',
-    titleJp: '憨憨操作',
-    voice: [
-      {
-        desCh: "一时的冲动",
-        desJp: "一时的冲动",
-        url: "kohigashihitona/8.mp3"
-      }
-    ]
-  }, {
-    titleCh: '憨鱼',
-    titleJp: '憨鱼',
-    voice: [
-      {
-        desCh: "阿巴阿巴rui",
-        desJp: "阿巴阿巴rui",
-        url: "kohigashihitona/小东的阿巴阿巴.mp3"
-      },
-      {
-        desCh: "ばび～",
-        desJp: "ばび～",
-        url: "kohigashihitona/我仿佛感觉小东在骂我.mp3"
-      },
-      {
-        desCh: "无脑mp3.mp3",
-        desJp: "无脑mp3.mp3",
-        url: "kohigashihitona/无脑mp3.mp3"
-      }
-    ]
-  }, {
-    titleCh: '你再骂？（请求）',
-    titleJp: '你再骂？（请求）',
-    voice: [
-      {
-        desCh: "你个没骨气的家伙",
-        desJp: "你个没骨气的家伙",
-        url: "kohigashihitona/这又是啥.mp3"
-      }
-    ]
-  }, {
-    titleCh: '其他',
-    titleJp: '其他',
-    voice: [
-      {
-        desCh: "小东的阿巴阿巴1分洗脑",
-        desJp: "小东的阿巴阿巴1分洗脑",
-        url: "kohigashihitona/小东的阿巴阿巴1分洗脑 (1).mp3"
-      },
-      {
-        desCh: "？（这又是哪位anti做的？）",
-        desJp: "？（这又是哪位anti做的？）",
-        url: "kohigashihitona/死机鱼.mp3"
-      }
-    ]
-  }, {
-    titleCh: '日常',
-    titleJp: '日常',
-    voice: [
-      {
-        desCh: "我开动了~",
-        desJp: "いただきま～す",
-        url: "kohigashihitona/5.mp3"
-      }
-    ]
-  }, {
-    titleCh: '谈花丸',
-    titleJp: '谈花丸',
-    voice: [
-      {
-        desCh: "晴琉琉喜欢欧式肉酱",
-        desJp: "晴琉琉喜欢欧式肉酱",
-        url: "kohigashihitona/7.mp3"
-      }
-    ]
-  }, {
-    titleCh: '无情机器',
-    titleJp: '无情机器',
-    voice: [
-      {
-        desCh: "嗯～这个真好吃",
-        desJp: "ん～うめーなこれな",
-        url: "kohigashihitona/8 (1).mp3"
-      },
-      {
-        desCh: "这里好恐怖好恐怖好恐怖",
-        desJp: "这里好恐怖好恐怖好恐怖",
-        url: "kohigashihitona/这说啥呢.mp3"
-      }
-    ]
-  }, {
-    titleCh: '嚣张小东',
-    titleJp: '嚣张小东',
-    voice: [
-      {
-        desCh: "鱼笑",
-        desJp: "鱼笑",
-        url: "kohigashihitona/呜哈哈.mp3"
-      }
-    ]
-  }, {
-    titleCh: '诱惑小东',
-    titleJp: '诱惑小东',
-    voice: [
-      {
-        desCh: "鱼娇喘（不是）",
-        desJp: "鱼娇喘（不是）",
-        url: "kohigashihitona/啊~新素材.mp3"
-      },
-      {
-        desCh: "哇变得越来越湿了怎么办啊",
-        desJp: "哇变得越来越湿了怎么办啊",
-        url: "kohigashihitona/这啥.mp3"
-      },
-      // {
-      //   desCh: "起床懒音",
-      //   desJp: "Twitterを使っています 「お布団から出たくないので声ガサガサ記録（？）残しておきます（さっき起きた",
-      //   url: "kohigashihitona/推特录音.mp3"
-      // },
-    ]
-  }, {
-    titleCh: '鱼叫',
-    titleJp: '鱼叫',
-    voice: [
-      {
-        desCh: "鱼叫",
-        desJp: "鱼叫",
-        url: "kohigashihitona/呜啊！啊↑哈↗哈↗.mp3"
-      },
-      {
-        desCh: "在哪儿呢！啊↑出现了！",
-        desJp: "在哪儿呢！啊↑出现了！",
-        url: "kohigashihitona/来听下说nm呢.mp3"
-      },
-      {
-        desCh: "还是鱼吸叫.mp3",
-        desJp: "还是鱼吸叫.mp3",
-        url: "kohigashihitona/还是鱼吸叫.mp3"
-      },
-      {
-        desCh: "注意耳膜",
-        desJp: "注意耳膜",
-        url: "kohigashihitona/鱼叫.mp3"
-      },
-      {
-        desCh: "鱼叫2",
-        desJp: "鱼叫2",
-        url: "kohigashihitona/鱼叫2.mp3"
-      },
-      {
-        desCh: "鱼吸叫",
-        desJp: "鱼吸叫",
-        url: "kohigashihitona/鱼吸叫.mp3"
-      }
-    ]
-  }, {
-    titleCh: '语气词',
-    titleJp: '语气词',
-    voice: [
-      {
-        desCh: "嘘~",
-        desJp: "嘘~",
-        url: "kohigashihitona/小东人鱼催尿音频.mp3"
-      },
-      {
-        desCh: "嘘↘",
-        desJp: "嘘↘",
-        url: "kohigashihitona/小东人鱼催尿音频2.mp3"
-      },
-    ]
-  }
+  kohiList: [
+    {
+      titleCh: '吃货小东',
+      titleJp: '吃货小东',
+      voice: [
+        {
+          desCh: "介绍一下商品",
+          desJp: "商品の説明をします",
+          url: "kohigashihitona/6 (1).mp3"
+        },
+        {
+          desCh: "人鱼我选择和风汉堡肉",
+          desJp: "ひとなおろしハンバーグだったじゃん、和風の",
+          url: "kohigashihitona/6.mp3"
+        },
+        {
+          desCh: "微波炉用500瓦热1分40秒",
+          desJp: "微波炉用500瓦热1分40秒",
+          url: "kohigashihitona/7 (1).mp3"
+        },
+        {
+          desCh: "711原创副食用商品",
+          desJp: "711原创副食用商品",
+          url: "kohigashihitona/9 (2).mp3"
+        },
+        {
+          desCh: "热完后渐渐变凉的时候",
+          desJp: "热完后渐渐变凉的时候",
+          url: "kohigashihitona/9.mp3"
+        }
+      ]
+    },
+    {
+      titleCh: '憨憨操作',
+      titleJp: '憨憨操作',
+      voice: [
+        {
+          desCh: "一时的冲动",
+          desJp: "一时的冲动",
+          url: "kohigashihitona/8.mp3"
+        }
+      ]
+    },
+    {
+      titleCh: '憨鱼',
+      titleJp: '憨鱼',
+      voice: [
+        {
+          desCh: "阿巴阿巴rui",
+          desJp: "阿巴阿巴rui",
+          url: "kohigashihitona/小东的阿巴阿巴.mp3"
+        },
+        {
+          desCh: "ばび～",
+          desJp: "ばび～",
+          url: "kohigashihitona/我仿佛感觉小东在骂我.mp3"
+        },
+        {
+          desCh: "无脑mp3.mp3",
+          desJp: "无脑mp3.mp3",
+          url: "kohigashihitona/无脑mp3.mp3"
+        }
+      ]
+    },
+    {
+      titleCh: '你再骂？（请求）',
+      titleJp: '你再骂？（请求）',
+      voice: [
+        {
+          desCh: "你个没骨气的家伙",
+          desJp: "你个没骨气的家伙",
+          url: "kohigashihitona/这又是啥.mp3"
+        }
+      ]
+    },
+    {
+      titleCh: '其他',
+      titleJp: '其他',
+      voice: [
+        {
+          desCh: "小东的阿巴阿巴1分洗脑",
+          desJp: "小东的阿巴阿巴1分洗脑",
+          url: "kohigashihitona/小东的阿巴阿巴1分洗脑 (1).mp3"
+        },
+        {
+          desCh: "？（这又是哪位anti做的？）",
+          desJp: "？（这又是哪位anti做的？）",
+          url: "kohigashihitona/死机鱼.mp3"
+        }
+      ]
+    },
+    {
+      titleCh: '日常',
+      titleJp: '日常',
+      voice: [
+        {
+          desCh: "我开动了~",
+          desJp: "いただきま～す",
+          url: "kohigashihitona/5.mp3"
+        }
+      ]
+    },
+    {
+      titleCh: '谈花丸',
+      titleJp: '谈花丸',
+      voice: [
+        {
+          desCh: "晴琉琉喜欢欧式肉酱",
+          desJp: "晴琉琉喜欢欧式肉酱",
+          url: "kohigashihitona/7.mp3"
+        }
+      ]
+    },
+    {
+      titleCh: '无情机器',
+      titleJp: '无情机器',
+      voice: [
+        {
+          desCh: "嗯～这个真好吃",
+          desJp: "ん～うめーなこれな",
+          url: "kohigashihitona/8 (1).mp3"
+        },
+        {
+          desCh: "这里好恐怖好恐怖好恐怖",
+          desJp: "这里好恐怖好恐怖好恐怖",
+          url: "kohigashihitona/这说啥呢.mp3"
+        }
+      ]
+    },
+    {
+      titleCh: '嚣张小东',
+      titleJp: '嚣张小东',
+      voice: [
+        {
+          desCh: "鱼笑",
+          desJp: "鱼笑",
+          url: "kohigashihitona/呜哈哈.mp3"
+        }
+      ]
+    },
+    {
+      titleCh: '诱惑小东',
+      titleJp: '诱惑小东',
+      voice: [
+        {
+          desCh: "鱼娇喘（不是）",
+          desJp: "鱼娇喘（不是）",
+          url: "kohigashihitona/啊~新素材.mp3"
+        },
+        {
+          desCh: "哇变得越来越湿了怎么办啊",
+          desJp: "哇变得越来越湿了怎么办啊",
+          url: "kohigashihitona/这啥.mp3"
+        },
+        // {
+        //   desCh: "起床懒音",
+        //   desJp: "Twitterを使っています 「お布団から出たくないので声ガサガサ記録（？）残しておきます（さっき起きた",
+        //   url: "kohigashihitona/推特录音.mp3"
+        // },
+      ]
+    },
+    {
+      titleCh: '鱼叫',
+      titleJp: '鱼叫',
+      voice: [
+        {
+          desCh: "鱼叫",
+          desJp: "鱼叫",
+          url: "kohigashihitona/呜啊！啊↑哈↗哈↗.mp3"
+        },
+        {
+          desCh: "在哪儿呢！啊↑出现了！",
+          desJp: "在哪儿呢！啊↑出现了！",
+          url: "kohigashihitona/来听下说nm呢.mp3"
+        },
+        {
+          desCh: "还是鱼吸叫.mp3",
+          desJp: "还是鱼吸叫.mp3",
+          url: "kohigashihitona/还是鱼吸叫.mp3"
+        },
+        {
+          desCh: "注意耳膜",
+          desJp: "注意耳膜",
+          url: "kohigashihitona/鱼叫.mp3"
+        },
+        {
+          desCh: "鱼叫2",
+          desJp: "鱼叫2",
+          url: "kohigashihitona/鱼叫2.mp3"
+        },
+        {
+          desCh: "鱼吸叫",
+          desJp: "鱼吸叫",
+          url: "kohigashihitona/鱼吸叫.mp3"
+        }
+      ]
+    },
+    {
+      titleCh: '语气词',
+      titleJp: '语气词',
+      voice: [
+        {
+          desCh: "嘘~",
+          desJp: "嘘~",
+          url: "kohigashihitona/小东人鱼催尿音频.mp3"
+        },
+        {
+          desCh: "嘘↘",
+          desJp: "嘘↘",
+          url: "kohigashihitona/小东人鱼催尿音频2.mp3"
+        },
+      ]
+    }
   ],
   //花丸
-  hanaList: [{
-    titleCh: '标题',
-    titleJp: '标题日语',
-    voice: [{
-      desCh: "鱼叫",
-      desJp: "ひとな",
-      url: "kohigashihitona/yujiao.mp3"
-    }, {
-      desCh: "鱼叫2",
-      desJp: "ひとな２",
-      url: "kohigashihitona/yujiao.mp3"
-    }, {
-      desCh: "鱼叫3",
-      desJp: "ひとな３",
-      url: "kohigashihitona/yujiao.mp3"
-    }]
-  },
-  {
-    titleCh: '标题2',
-    titleJp: '标题日语2',
-    voice: [{
-      desCh: "鱼叫",
-      desJp: "ひとな",
-      url: "kohigashihitona/yujiao.mp3"
-    }, {
-      desCh: "鱼叫2",
-      desJp: "ひとな２",
-      url: "kohigashihitona/yujiao.mp3"
-    }, {
-      desCh: "鱼叫3",
-      desJp: "ひとな３",
-      url: "kohigashihitona/yujiao.mp3"
-    }]
-  }
+  hanaList: [
+    {
+      titleCh: '标题',
+      titleJp: '标题日语',
+      voice: [
+        {
+          desCh: "鱼叫",
+          desJp: "ひとな",
+          url: "kohigashihitona/yujiao.mp3"
+        },
+        {
+          desCh: "鱼叫2",
+          desJp: "ひとな２",
+          url: "kohigashihitona/yujiao.mp3"
+        },
+        {
+          desCh: "鱼叫3",
+          desJp: "ひとな３",
+          url: "kohigashihitona/yujiao.mp3"
+        }
+      ]
+    },
+    {
+      titleCh: '标题2',
+      titleJp: '标题日语2',
+      voice: [
+        {
+          desCh: "鱼叫",
+          desJp: "ひとな",
+          url: "kohigashihitona/yujiao.mp3"
+        },
+        {
+          desCh: "鱼叫2",
+          desJp: "ひとな２",
+          url: "kohigashihitona/yujiao.mp3"
+        },
+        {
+          desCh: "鱼叫3",
+          desJp: "ひとな３",
+          url: "kohigashihitona/yujiao.mp3"
+        }]
+    }
   ],
   //鹿乃
-  kanoList: [{
-    titleCh: '标题',
-    titleJp: '标题日语',
-    voice: [{
-      desCh: "鱼叫",
-      desJp: "ひとな",
-      url: "kohigashihitona/yujiao.mp3"
-    }, {
-      desCh: "鱼叫2",
-      desJp: "ひとな２",
-      url: "kohigashihitona/yujiao.mp3"
-    }, {
-      desCh: "鱼叫3",
-      desJp: "ひとな３",
-      url: "kohigashihitona/yujiao.mp3"
-    }]
-  },
-  {
-    titleCh: '标题2',
-    titleJp: '标题日语2',
-    voice: [{
-      desCh: "鱼叫",
-      desJp: "ひとな",
-      url: "kohigashihitona/yujiao.mp3"
-    }, {
-      desCh: "鱼叫2",
-      desJp: "ひとな２",
-      url: "kohigashihitona/yujiao.mp3"
-    }, {
-      desCh: "鱼叫3",
-      desJp: "ひとな３",
-      url: "kohigashihitona/yujiao.mp3"
-    }]
-  }
+  kanoList: [
+    {
+      titleCh: '标题',
+      titleJp: '标题日语',
+      voice: [
+        {
+          desCh: "鱼叫",
+          desJp: "ひとな",
+          url: "kohigashihitona/yujiao.mp3"
+        },
+        {
+          desCh: "鱼叫2",
+          desJp: "ひとな２",
+          url: "kohigashihitona/yujiao.mp3"
+        },
+        {
+          desCh: "鱼叫3",
+          desJp: "ひとな３",
+          url: "kohigashihitona/yujiao.mp3"
+        }
+      ]
+    },
+    {
+      titleCh: '标题2',
+      titleJp: '标题日语2',
+      voice: [
+        {
+          desCh: "鱼叫",
+          desJp: "ひとな",
+          url: "kohigashihitona/yujiao.mp3"
+        },
+        {
+          desCh: "鱼叫2",
+          desJp: "ひとな２",
+          url: "kohigashihitona/yujiao.mp3"
+        },
+        {
+          desCh: "鱼叫3",
+          desJp: "ひとな３",
+          url: "kohigashihitona/yujiao.mp3"
+        }
+      ]
+    }
   ],
   //ののの
-  nonoList: [{
-    titleCh: '标题',
-    titleJp: '标题日语',
-    voice: [{
-      desCh: "鱼叫",
-      desJp: "ひとな",
-      url: "kohigashihitona/yujiao.mp3"
-    }, {
-      desCh: "鱼叫2",
-      desJp: "ひとな２",
-      url: "kohigashihitona/yujiao.mp3"
-    }, {
-      desCh: "鱼叫3",
-      desJp: "ひとな３",
-      url: "kohigashihitona/yujiao.mp3"
-    }]
-  },
-  {
-    titleCh: '标题2',
-    titleJp: '标题日语2',
-    voice: [{
-      desCh: "鱼叫",
-      desJp: "ひとな",
-      url: "kohigashihitona/yujiao.mp3"
-    }, {
-      desCh: "鱼叫2",
-      desJp: "ひとな２",
-      url: "kohigashihitona/yujiao.mp3"
-    }, {
-      desCh: "鱼叫3",
-      desJp: "ひとな３",
-      url: "kohigashihitona/yujiao.mp3"
-    }]
-  }
+  nonoList: [
+    {
+      titleCh: '标题',
+      titleJp: '标题日语',
+      voice: [
+        {
+          desCh: "鱼叫",
+          desJp: "ひとな",
+          url: "kohigashihitona/yujiao.mp3"
+        },
+        {
+          desCh: "鱼叫2",
+          desJp: "ひとな２",
+          url: "kohigashihitona/yujiao.mp3"
+        },
+        {
+          desCh: "鱼叫3",
+          desJp: "ひとな３",
+          url: "kohigashihitona/yujiao.mp3"
+        }
+      ]
+    },
+    {
+      titleCh: '标题2',
+      titleJp: '标题日语2',
+      voice: [
+        {
+          desCh: "鱼叫",
+          desJp: "ひとな",
+          url: "kohigashihitona/yujiao.mp3"
+        },
+        {
+          desCh: "鱼叫2",
+          desJp: "ひとな２",
+          url: "kohigashihitona/yujiao.mp3"
+        },
+        {
+          desCh: "鱼叫3",
+          desJp: "ひとな３",
+          url: "kohigashihitona/yujiao.mp3"
+        }
+      ]
+    }
   ]
 }
